b565130fda4885b599227435ac2ea697018f856f
Your Name <you@example.com>
2024-12-12 16:48:40 +0800

    change wget

--- setup/make.sh
+++ setup/make.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 cd /tmp || exit 1
-axel -a -n 10 https://ftp.gnu.org/gnu/make/make-"${1:?}".tar.gz
-tar xvzf /tmp/make-"${1:?}".tar.gz
+wget https://ftp.gnu.org/gnu/make/make-"${1:?}".tar.gz
+tar xvzf /tmp/make-"${1:?}".tar.gz.1
 cd /tmp/make-"${1:?}" || exit 1
 ./configure
 bash ./build.sh
